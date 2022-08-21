7cfa26c167515f73d86e0326826482bf3aa5406b
zjlhxq <hadoop.jz@qq.com>
2022-08-21 17:40:29 -0400

    ELK stack for production

--- deployStack.sh
+++ deployStack.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-export ELASTIC_VERSION=7.9.1
-export ELASTICSEARCH_USERNAME=elastic
-export ELASTICSEARCH_PASSWORD=changeme
-export ELASTICSEARCH_HOST=nextgen
-export INITIAL_MASTER_NODES=nextgen
-
-docker network create --driver overlay --attachable elastic
+docker network create --driver overlay --attachable elk_network
 docker stack deploy --compose-file docker-compose.yml elastic
