b75bd7b38219729794118169b12d5064a4050aa9
Du Junxi <junxi.du@sms-group.com>
2022-08-24 09:38:00 +0800

    Initial version of production deployment

--- deployStack.sh
+++ deployStack.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-docker network create --driver overlay --attachable elk_network
-docker stack deploy --compose-file docker-compose.yml elastic
+export ELASTICSEARCH_USERNAME="elastic"
+export ELASTICSEARCH_PASSWORD="changeme"
+export ELASTICSEARCH_HOST="ccm-l2-kafka-node1"
+export KIBANA_HOST="ccm-l2-kafka-node2"
+export INITIAL_MASTER_NODES="ccm-l2-kafka-node1,ccm-l2-kafka-node2,ccm-l2-kafka-node3"
+export ELASTIC_VERSION="7.17.5"
+export DOCKER_REPO="docker.nextgen.io:5000/"
+
+
+docker stack deploy --compose-file docker-compose.yml --with-registry-auth elastic
