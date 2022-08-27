82285a89018f02c19e026e46de6f68e08a3e350b
Du Junxi <junxi.du@sms-group.com>
2022-08-28 07:17:22 +0800

    Fixed the issue with starting up ES services, but the filebeats are not working yet

--- deployStack.sh
+++ deployStack.sh
@@ -6,6 +6,7 @@ export ELASTICSEARCH_HOST="ccm-l2-kafka-node1"
 export KIBANA_HOST="ccm-l2-kafka-node2"
 export INITIAL_MASTER_NODES="ccm-l2-kafka-node1,ccm-l2-kafka-node2,ccm-l2-kafka-node3"
 export ELASTIC_VERSION="7.17.5"
+#export ELASTIC_VERSION="8.3.3"
 export DOCKER_REPO="docker.nextgen.io:5000/"
 
 
