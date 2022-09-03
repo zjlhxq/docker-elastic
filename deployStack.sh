29fee54ea27a082e9778bb7f8d4a60f1a2803a37
Du Junxi <junxi.du@sms-group.com>
2022-09-03 23:49:00 +0800

    Fix the issue with too big replication-factor:22 set to es connect service

--- deployStack.sh
+++ deployStack.sh
@@ -8,6 +8,7 @@ export INITIAL_MASTER_NODES="ccm-l2-kafka-node1,ccm-l2-kafka-node2,ccm-l2-kafka-
 export ELASTIC_VERSION="7.17.5"
 #export ELASTIC_VERSION="8.3.3"
 export DOCKER_REPO="docker.nextgen.io:5000/"
-
+export BOOTSTRAP_SERVERS="ccm-l2-kafka-node1:9092,ccm-l2-kafka-node2:9092,ccm-l2-kafka-node3:9092"
+export SCHEMA_REGISTRY_URL="http://ccm-l2-kafka-node1:8081,http://ccm-l2-kafka-node2:8081,http://ccm-l2-kafka-node3:8081"
 
 docker stack deploy --compose-file docker-compose.yml --with-registry-auth elastic
