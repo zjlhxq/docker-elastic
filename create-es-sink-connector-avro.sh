8889f6297866c5374203a28c3b78f4d0f323a0f7
Du Junxi <junxi.du@sms-group.com>
2022-09-04 06:14:51 +0800

    Add ES indices for tcp inbound deserialized topics

--- create-es-sink-connector-avro.sh
+++ create-es-sink-connector-avro.sh
@@ -5,18 +5,18 @@ export SCHEMA_REGISTRY_URL="http://ccm-l2-kafka-node1:8081"
 generate_payload() {
 cat <<EOF
 {
-  "name": "elasticsearch-sink-connector",
+  "name": "elasticsearch-sink-connector-avro-source",
   "config": {
     "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
     "connection.url": "http://ccm-l2-kafka-node1:9200",
-    "topics": "ccm1-tcp-inbound-raw-data,ccm2-tcp-inbound-raw-data",
+    "topics": "ccm1-tcp-inbound-data-deserialized,ccm2-tcp-inbound-data-deserialized",
     "key.ignore": "true",
     "tasks.max": 1,
     "type.name": "_doc", 
-    "value.converter": "org.apache.kafka.connect.converters.ByteArrayConverter",
-    "transforms": "Cast",
-    "transforms.Cast.type": "org.apache.kafka.connect.transforms.Cast\$Value",
-    "transforms.Cast.spec": "string",
+    "value.converter": "io.confluent.connect.avro.AvroConverter",
+    "value.converter.schema.registry.url": "$SCHEMA_REGISTRY_URL",
+    "value.converter.value.subject.name.strategy": "io.confluent.kafka.serializers.subject.RecordNameStrategy",
+    "value.converter.enhanced.avro.schema.support": true,
     "behavior.on.malformed.documents": "ignore"
   }
 }
