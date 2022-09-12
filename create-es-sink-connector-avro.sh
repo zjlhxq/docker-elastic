8ff62a6e6d642fbf8aac7f549d05fa41d544200e
Du Junxi <junxi.du@sms-group.com>
2022-09-12 11:11:39 +0800

    Added timestamps to all messages consumed from kafka topics

--- create-es-sink-connector-avro.sh
+++ create-es-sink-connector-avro.sh
@@ -5,27 +5,31 @@ export SCHEMA_REGISTRY_URL="http://ccm-l2-kafka-node1:8081"
 generate_payload() {
 cat <<EOF
 {
-  "name": "es-sink-connector-avro",
+  "name": "es-sink-connector-canonical-E",
   "config": {
     "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
     "connection.url": "http://ccm-l2-kafka-node1:9200",
-    "topics": "ccm1-tcp-inbound-data-deserialized,ccm2-tcp-inbound-data-deserialized,ccm1-plc-inbound-raw-data,ccm2-plc-inbound-raw-data,ccm1-plc-inbound-raw-data-passthrough,ccm2-plc-inbound-raw-data-passthrough,ccm1-event-log-records,ccm2-event-log-records",
+    "topics": "ccm1-tcp-inbound-data-deserialized,ccm2-tcp-inbound-data-deserialized,ccm1-plc-inbound-raw-data,ccm2-plc-inbound-raw-data,ccm1-plc-inbound-raw-data-passthrough,ccm2-plc-inbound-raw-data-passthrough,ccm1-event-log-records,ccm2-event-log-records,ccm1-tcp-outbound-data-processed,ccm2-tcp-outbound-data-processed,ccm1-plc-outbound--tcs-hsa-s1,ccm1-plc-outbound--tcs-hsa-s2,ccm1-plc-outbound--tcs-nonhsa-s1,ccm1-plc-outbound--tcs-nonhsa-s2,ccm1-plc-outbound-common,ccm1-plc-outbound-data,ccm1-plc-outbound-drive-tcm-marker-s1,ccm1-plc-outbound-drive-tcm-marker-s2,ccm1-plc-outbound-instrument-s1,ccm1-plc-outbound-instrument-s2,ccm1-plc-outbound-raw-data,ccm1-plc-outbound-raw-data-common,ccm1-plc-outbound-raw-data-strand1,ccm1-plc-outbound-raw-data-strand2,ccm1-plc-outbound-tcs-hsa-s1,ccm1-plc-outbound-tcs-hsa-s2,ccm1-plc-outbound-tcs-nonhsa-s1,ccm1-plc-outbound-tcs-nonhsa-s2,ccm2-plc-outbound--tcs-hsa-s1,ccm2-plc-outbound--tcs-hsa-s2,ccm2-plc-outbound--tcs-nonhsa-s1,ccm2-plc-outbound--tcs-nonhsa-s2,ccm2-plc-outbound-common,ccm2-plc-outbound-data,ccm2-plc-outbound-drive-tcm-marker-s2,ccm2-plc-outbound-drive-tcm-marker-s2,ccm2-plc-outbound-instrument-s1,ccm2-plc-outbound-instrument-s2,ccm2-plc-outbound-raw-data,ccm2-plc-outbound-raw-data-common,ccm2-plc-outbound-raw-data-strand1,ccm2-plc-outbound-raw-data-strand2,ccm2-plc-outbound-tcs-hsa-s1,ccm2-plc-outbound-tcs-hsa-s2,ccm2-plc-outbound-tcs-nonhsa-s1,ccm2-plc-outbound-tcs-nonhsa-s2",
     "key.ignore": "true",
-    "tasks.max": 1,
+    "schema.ignore": "false",
+    "tasks.max": 5,
     "type.name": "_doc", 
-    "transforms": "InsertTimestamp,ConvertTimestamp",
-    "transforms.InsertTimestamp.type": "org.apache.kafka.connect.transforms.InsertField\$Value",
-    "transforms.InsertTimestamp.timestamp.field": "@timestamp",
+    "transforms": "ExtractTimestamp,ConvertTimestamp",
+    "transforms.ExtractTimestamp.type": "org.apache.kafka.connect.transforms.InsertField\$Value",
+    "transforms.ExtractTimestamp.timestamp.field": "@timestamp",
     "transforms.ConvertTimestamp.type": "org.apache.kafka.connect.transforms.TimestampConverter\$Value",
     "transforms.ConvertTimestamp.field": "@timestamp",
     "transforms.ConvertTimestamp.format": "yyyy-MM-dd'T'HH:mm:ss'Z'",
-    "transforms.ConvertTimestamp.target.type": "string",
+    "transforms.ConvertTimestamp.target.type": "Timestamp",
     "value.converter": "io.confluent.connect.avro.AvroConverter",
     "value.converter.schema.registry.url": "$SCHEMA_REGISTRY_URL",
     "value.converter.value.subject.name.strategy": "io.confluent.kafka.serializers.subject.RecordNameStrategy",
-    "value.converter.enhanced.avro.schema.support": true,
+    "value.converter.enhanced.avro.schema.support": "true",
     "consumer.auto.offset.reset": "earliest",
-    "behavior.on.malformed.documents": "ignore"
+    "behavior.on.malformed.documents": "warn",
+    "errors.tolerance": "all",
+    "errors.log.enable": "true",
+    "errors.log.include,message": "true"
   }
 }
 EOF
--- create-es-sink-connector-tcp.sh
+++ create-es-sink-connector-tcp.sh
@@ -5,7 +5,7 @@ export SCHEMA_REGISTRY_URL="http://ccm-l2-kafka-node1:8081"
 generate_payload() {
 cat <<EOF
 {
-  "name": "es-sink-connector-tcp-raw",
+  "name": "es-sink-connector-raw-E",
   "config": {
     "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
     "connection.url": "http://ccm-l2-kafka-node1:9200",
@@ -14,20 +14,20 @@ cat <<EOF
     "tasks.max": 1,
     "consumer.auto.offset.reset": "earliest",
     "flush.synchronously": "true",
-    "type.name": "kafka-connect", 
+    "type.name": "_doc", 
     "value.converter": "org.apache.kafka.connect.storage.StringConverter",
     "transforms": "MakeMap,InsertSource,InsertTimestamp,ConvertTimestamp",
     "transforms.MakeMap.type": "org.apache.kafka.connect.transforms.HoistField\$Value",
     "transforms.MakeMap.field": "message",
     "transforms.InsertSource.type": "org.apache.kafka.connect.transforms.InsertField\$Value",
     "transforms.InsertSource.static.field": "data_source",
-    "transforms.InsertSource.static.value": "tcp-source",
+    "transforms.InsertSource.static.value": "tcp",
     "transforms.InsertTimestamp.type": "org.apache.kafka.connect.transforms.InsertField\$Value",
     "transforms.InsertTimestamp.timestamp.field": "@timestamp",
     "transforms.ConvertTimestamp.type": "org.apache.kafka.connect.transforms.TimestampConverter\$Value",
     "transforms.ConvertTimestamp.field": "@timestamp",
     "transforms.ConvertTimestamp.format": "yyyy-MM-dd'T'HH:mm:ss'Z'",
-    "transforms.ConvertTimestamp.target.type": "string",
+    "transforms.ConvertTimestamp.target.type": "Timestamp",
     "behavior.on.malformed.documents": "ignore"
   }
 }
